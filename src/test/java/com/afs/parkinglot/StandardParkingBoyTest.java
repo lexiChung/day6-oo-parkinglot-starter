b4a02e4d057c2e294afbf85fdf6721de6b7c7eaa
Lexi Zhong <zhongle5@oocl.com>
2025-09-08 21:04:10 +0800

    test: 通过parkingBoy进行fetch操作

--- src/test/java/com/afs/parkinglot/StandardParkingBoyTest.java
+++ src/test/java/com/afs/parkinglot/StandardParkingBoyTest.java
@@ -2,6 +2,7 @@ package com.afs.parkinglot;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class StandardParkingBoyTest {
@@ -13,4 +14,15 @@ public class StandardParkingBoyTest {
         Ticket ticket = parkingBoy.parking(car);
         assertNotNull(ticket);
     }
+
+    @Test
+    void should_return_car_when_fetching_given_parking_boy_and_ticket(){
+        ParkingLot parkingLot = new ParkingLot(10);
+        StandardParkingBoy parkingBoy = new StandardParkingBoy(parkingLot);
+        Car car = new Car();
+        Ticket ticket = parkingBoy.parking(car);
+        Car fetchedCar = parkingBoy.fetching(ticket);
+        assertEquals(car, fetchedCar);
+    }
 }
+
