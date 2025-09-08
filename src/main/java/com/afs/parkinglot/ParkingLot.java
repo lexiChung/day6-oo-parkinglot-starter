a224284f9a76789e66f2b2aeef7204bc6722aeb1
Lexi Zhong <zhongle5@oocl.com>
2025-09-08 20:07:23 +0800

    feat: 有位置时允许停车并返回ticket

--- src/main/java/com/afs/parkinglot/ParkingLot.java
+++ src/main/java/com/afs/parkinglot/ParkingLot.java
@@ -1,4 +1,23 @@
 package com.afs.parkinglot;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class ParkingLot {
+    private int capacity;
+    private Map<Ticket,Car> ticketCarMap = new HashMap<>();
+
+    public ParkingLot(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public Ticket parking(Car car) {
+        if(capacity<=0) throw new RuntimeException("No available position.");
+        Ticket ticket = new Ticket();
+        ticketCarMap.put(ticket,car);
+        capacity--;
+        return ticket;
+    }
 }
--- src/test/java/com/afs/parkinglot/ParkingLotTest.java
+++ src/test/java/com/afs/parkinglot/ParkingLotTest.java
@@ -1,4 +1,17 @@
 package com.afs.parkinglot;
 
-public class ParkingLotTest {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class ParkingLotTest {
+    @Test
+    void should_return_ticket_when_parking_given_lot_and_car(){
+        ParkingLot parkingLot = new ParkingLot(10);
+        Car car = new Car();
+        Ticket ticket = parkingLot.parking(car);
+        assertNotNull(ticket);
+    }
+
+
 }
